c62d78d9af5bee3af6f282c3d50846159b817618
Shakthi Prashanth M <prashanth.shakthi.m@gmail.com>
2018-08-26 05:37:28 +0530

    Update print_matrix_spirally.cc

--- print_matrix_spirally.cc
+++ print_matrix_spirally.cc
@@ -1,93 +1,111 @@
+/*
+ * Print Matrix Spirally
+ * Author: Shakthi Prashanth <m.shakthi.prashanth@gmail.com>
+ * Date: 25/08/2018
+*/
+
+
 #include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <cmath>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
-#define ROWS    100
-#define COLS    100
-
+using Row = vector<int>;
 using Cell = pair<int, int>;
 
-Cell  printRight(int a[][COLS], int i, int j, int rows, int cols, int pass, size_t &nVisited);
-Cell  printDown(int a[][COLS], int i, int j, int rows, int cols, int pass, size_t &nVisited);
-Cell  printLeft(int a[][COLS], int i, int j, int rows, int cols, int pass, size_t &nVisited);
-Cell  printUp(int a[][COLS], int i, int j, int rows, int cols, int pass, size_t &nVisited);
+vector<Row>   readMatrix(int rows, int cols); // Linked to read_matrix.cc
 
-void printSpirally(int matrix[][COLS], int rows, int cols)
+Cell          printRight(const vector<Row> &matrix, int i, int j, int pass, size_t &nVisited);
+Cell          printDown(const vector<Row> &matrix, int i, int j, int pass, size_t &nVisited);
+Cell          printLeft(const vector<Row> &matrix, int i, int j, int pass, size_t &nVisited);
+Cell          printUp(const vector<Row> &matrix, int i, int j, int pass, size_t &nVisited);
+
+void printSpirally(const vector<Row> &matrix)
 {
   size_t nVisited = 0;
+  auto rows = matrix.size(), cols = matrix[0].size();
 
-  for (int pass = 1, i = 0, j = 0; nVisited < size_t(rows*cols); pass++, i++, j++) {
-    auto lastCell = printRight(matrix, i, j, rows, cols, pass, nVisited);
-    if (nVisited >= rows*cols)	break;
-    
-    lastCell = printDown(matrix, lastCell.first+1, lastCell.second, rows, cols, pass, nVisited);
-	if (nVisited >= rows*cols)	break;
-    
-    lastCell = printLeft(matrix, lastCell.first, lastCell.second-1, rows, cols, pass, nVisited);
-    if (nVisited >= rows*cols)	break;
-    
-    lastCell = printUp(matrix, lastCell.first-1, lastCell.second, rows, cols, pass, nVisited);
-    if (nVisited >= rows*cols)	break;
+  for (int pass = 1, i = 0, j = 0; nVisited < rows*cols; pass++, i++, j++) {
+    auto lastCell = printRight(matrix, i, j, pass, nVisited);
+    lastCell = printDown(matrix, lastCell.first+1, lastCell.second, pass, nVisited);
+    lastCell = printLeft(matrix, lastCell.first, lastCell.second-1, pass, nVisited);
+    lastCell = printUp(matrix, lastCell.first-1, lastCell.second, pass, nVisited);
   }
   //cout << "nVisited: " << nVisited << endl;
 }
 
-Cell printRight(int matrix[][COLS], int i, int j, int rows, int cols, int pass, size_t &nVisited)
+Cell printRight(const vector<Row> &matrix, int i, int j, int pass, size_t &nVisited)
 {
+  auto cols = matrix[i].size();
   auto count = cols - (pass-1)*2;
-  //cerr << i << ", " << j << "cols: " << cols << ", count" << count << endl;
+  cerr << i << ", " << j << "cols: " << cols << ", count" << count << endl;
 
   while (count--) {
     cout << matrix[i][j++];
-    nVisited++;
-    if (nVisited == size_t(rows*cols)) cout << endl;
+    if (count == 0) cout << endl;
     else cout << ", ";
+    nVisited++;
   }
   //cout << " R nVisited: " << nVisited << endl;
   return make_pair(i, j-1); // last cell visited
 }
 
-Cell printDown(int matrix[][COLS], int i, int j, int rows, int cols, int pass, size_t &nVisited)
+Cell printDown(const vector<Row> &matrix, int i, int j, int pass, size_t &nVisited)
 {
+  auto rows = matrix.size();
   auto count = pass == 1 ? rows-1 : rows-1-pass;
 
   while (count--) {
     cout << matrix[i++][j];
-    nVisited++;
-    if (nVisited == size_t(rows*cols)) cout << endl;
+    if (count == 0) cout << endl;
     else cout << ", ";
+    nVisited++;
   }
 //  cout << " D nVisited: " << nVisited << endl;
   return make_pair(i-1, j); // last cell visited
 }
 
-Cell printLeft(int matrix[][COLS], int i, int j, int rows, int cols, int pass, size_t &nVisited)
+Cell printLeft(const vector<Row> &matrix, int i, int j, int pass, size_t &nVisited)
 {
+  auto cols = matrix[i].size();
   auto count = pass == 1 ? cols-1 : cols-1-pass;
 
   while (count--) {
     cout << matrix[i][j--];
-	nVisited++;
-    if (nVisited == size_t(rows*cols)) cout << endl;
+    if (count == 0) cout << endl;
     else cout << ", ";
+    nVisited++;
   }
 //  cout << " L nVisited: " << nVisited << endl;
   return make_pair(i, j+1); // last cell visited
 }
 
-Cell printUp(int matrix[][COLS], int i, int j, int rows, int cols, int pass, size_t &nVisited)
+Cell printUp(const vector<Row> &matrix, int i, int j, int pass, size_t &nVisited)
 {
+  auto rows = matrix.size();
   auto count = pass == 1 ? rows-2 : rows-2-pass;
 
   while (count--) {
     cout << matrix[i--][j];
-    nVisited++;
-    if (nVisited == size_t(rows*cols)) cout << endl;
+    if (count == 0) cout << endl;
     else cout << ", ";
+    nVisited++;
   }
 //  cout << " U nVisited: " << nVisited << endl;
   return make_pair(i+1, j); // last cell visited
 }
+
+int main()
+{
+  cout << "Enter matrix size: rows, cols" << endl;
+
+  pair<int, int> size;
+  cin >> size.first >> size.second;
+
+  cout << "Enter elements of matrix[" << size.first << "][" << size.second << "]" << endl;
+  auto matrix = readMatrix(size.first, size.second);
+  cout << "Read matrix" << endl;
+  printSpirally(matrix);
+}
+
